9aeb8df20684849f0c009a122d6f8f9269024e7d
Vishnu Suresh <vishnusuresh683@gmail.com>
2026-06-05 17:10:57 +0530

    Add product wishlist feature

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -12,15 +12,15 @@ const {
 const protect = require("../middleware/authMiddleware");
 const upload = require("../middleware/uploadMiddleware");
 
+// Wishlist routes (must be before /:id)
+router.get("/wishlist", protect, getWishlist);
+router.post("/wishlist/:id", protect, addToWishlist);
+router.delete("/wishlist/:id", protect, removeFromWishlist);
+
 // Product routes
 router.post("/", protect, upload.array("images", 10), createProduct);
 router.put("/:id", protect, upload.array("images", 10), updateProduct);
 router.get("/", getProducts);
 router.get("/:id", getProductById);
-
-// Wishlist routes
-router.get("/wishlist", protect, getWishlist);
-router.post("/wishlist/:id", protect, addToWishlist);
-router.delete("/wishlist/:id", protect, removeFromWishlist);
 
 module.exports = router;
